487b3d0fe4901a383adfc1534d7125ad96adbfe8
arGna22 <ag3nzqp00@protonmail.com>
2026-05-31 10:45:52 -0400

    Created a main.c for testing

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "heap_internal.h"
+#include <stdio.h>
+
+int main() {
+    int return_value = heap_init();
+
+    if (return_value == 0) {
+        printf("So far, success!\n");
+    }
+    return 0;
+}
