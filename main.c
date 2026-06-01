34331af55acf244dc84394d01a0458704b9c84cb
arGna22 <ag3nzqp00@protonmail.com>
2026-06-01 13:11:49 -0400

    Update main.c for testing memory allocation functionality

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include "heap_internal.h"
 #include "utils.h"
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     int return_value = _heap_init();
+
+    char* mystr = (char*)heap_alloc(12);
+    strcpy(mystr, "hello!");
+    printf("%s\n", mystr);
     return 0;
 }
