a8e64c25ba34ad5450efc0598bd11f136c4384c1
arGna22 <ag3nzqp00@protonmail.com>
2026-07-25 15:09:38 -0400

    Create test for address map initialization, hashing, and basic insertion

---
+++ addr_map_test.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <sys/mman.h>
+#include "addr_map.h"
+#include "heap_internal.h"
+
+int main() 
+{
+
+    
+
+    initialize_hash_map();
+
+
+    struct slab slab_metadata = { 0 }; 
+
+
+    for (int i = 0; i < 8; i++) {
+        void* my_addr = mmap(
+            NULL, 
+            4096,
+            PROT_READ | PROT_WRITE,
+            MAP_PRIVATE | MAP_ANON,
+            -1, 
+            0
+        );
+        addr_map_insert(my_addr, slab_metadata);
+        printf("-----\n");
+        addr_map_enumerate();
+    }
+
+    return 0;
+}
