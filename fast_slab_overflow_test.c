b32ac3ab545c0da59a773421d9cfdd1a4b6b3bc0
arGna22 <ag3nzqp00@protonmail.com>
2026-07-27 14:00:12 -0400

    Add build for fast slab overflow test

--- fast_slab_overflow_test.c
+++ fast_slab_overflow_test.c
@@ -1,4 +1,5 @@
 #include "heap_internal.h"
+#include "utils.h"
 
 #include <assert.h>
 #include <stdint.h>
@@ -9,10 +10,12 @@
 #define ALLOCATION_COUNT 8192
 #define VERIFY_INTERVAL 128
 #define MAX_FAST_SIZE 2048
+#define TEST_PAGE_SIZE 4096
 
 struct allocation {
     void *ptr;
-    size_t size;
+    size_t requested_size;
+    size_t usable_size;
     unsigned char pattern;
 };
 
@@ -26,16 +29,28 @@ static unsigned char pattern_for(size_t index, size_t size)
     return (unsigned char)((index * 131u + size * 17u + 0x5au) & 0xffu);
 }
 
+static void verify_slot_bounds(const struct allocation *alloc)
+{
+    uintptr_t address = (uintptr_t)alloc->ptr;
+    size_t page_offset = address & (TEST_PAGE_SIZE - 1);
+
+    assert(alloc->usable_size >= alloc->requested_size);
+    assert((page_offset % alloc->usable_size) == 0);
+    assert(page_offset + alloc->usable_size <= TEST_PAGE_SIZE);
+}
+
 static void fill_allocation(const struct allocation *alloc)
 {
-    memset(alloc->ptr, alloc->pattern, alloc->size);
+    memset(alloc->ptr, alloc->pattern, alloc->usable_size);
 }
 
 static void verify_allocation(const struct allocation *alloc)
 {
     unsigned char *p = alloc->ptr;
 
-    for (size_t i = 0; i < alloc->size; i++) {
+    verify_slot_bounds(alloc);
+
+    for (size_t i = 0; i < alloc->usable_size; i++) {
         assert(p[i] == alloc->pattern);
     }
 }
@@ -54,11 +69,15 @@ int main(void)
     srand(0x51ab500u);
 
     for (size_t i = 0; i < ALLOCATION_COUNT; i++) {
-        allocs[i].size = random_fast_size();
-        allocs[i].ptr = heap_alloc(allocs[i].size);
-        allocs[i].pattern = pattern_for(i, allocs[i].size);
+        allocs[i].requested_size = random_fast_size();
+        allocs[i].usable_size =
+            (size_t)determine_size_class((int)allocs[i].requested_size);
+        allocs[i].ptr = heap_alloc(allocs[i].requested_size);
+        allocs[i].pattern =
+            pattern_for(i, allocs[i].requested_size);
 
         assert(allocs[i].ptr != NULL);
+        verify_slot_bounds(&allocs[i]);
         fill_allocation(&allocs[i]);
 
         if ((i % VERIFY_INTERVAL) == 0) {
