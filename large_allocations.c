e1e1ab5fb7d079cd9e6cf85e861db74deee3b7c9
arGna22 <ag3nzqp00@protonmail.com>
2026-08-03 17:04:59 -0400

    Implement hardened large memory allocation and free, using guarded regions

---
+++ large_allocations.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "addr_map.h"
+#include "large_allocations.h"
+#include "secure_utils.h"
+
+void* hardened_large_malloc(size_t payload_size) {
+    struct guarded_region region;
+
+    region = create_gaurded_region(payload_size); 
+    struct large_meta large_metadata;
+
+    large_metadata.mmap_base = region.mmap_base;
+    large_metadata.payload_size = payload_size;
+    large_metadata.total_size = region.total_size;
+
+    struct map_value map_value = construct_map_value(NULL, &large_metadata);
+    addr_map_insert(LARGE, region.usable_ptr, map_value);
+
+    return region.usable_ptr;
+}
+
+void hardened_large_free(void* ptr) {
+
+    struct map_entry* map_entry = addr_map_lookup(LARGE, (uintptr_t)ptr);
+
+    if (map_entry == NULL)
+        return;
+
+    struct guarded_region region;
+    region.mmap_base = map_entry->value.large.mmap_base;
+    region.total_size = map_entry->value.large.total_size;
+    region.usable_ptr = ptr;
+
+    destroy_guarded_region(&region);
+}
