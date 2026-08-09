e2d341569c044184eb22c324f593014c25a6ab03
arGna22 <ag3nzqp00@protonmail.com>
2026-08-09 17:29:54 -0400

    Update TODO and clean up heap_internal.h

--- heap_internal.h
+++ heap_internal.h
@@ -16,39 +16,26 @@ struct span
 /* Structs for slabs and large chunks */
 struct slab {
     void *base;
-    size_t size_class;
-    size_t slot_count; // How many available slots there are
-    size_t free_count; // How many of these slots are free?
-    void* start_of_next_slot;
-    uint64_t alloc_bitmap[4];
+    uint16_t size_class;
+    uint16_t slot_count; // How many available slots there are
+    uint16_t free_count; // How many of these slots are free?
+//    uint64_t alloc_bitmap[4];
     uint8_t alloc_bytemap[256];
     struct slab *next;
     struct slab *prev;
-};
 
-/* Structs for large chunks */
-struct large_chunk
-{
-    struct span span;
-    size_t prev_size;
-    int allocated; // 0 if free, 1 if allocated. 
-    struct large_chunk *fd;
-    struct large_chunk *bk;
-    uint64_t size;
+    /* Fisher-Yates Array */
+    uint16_t indicies[];
 };
 
 /* Internal heap functions */
 int _heap_init();
 void _allocate_fast_bin_page(int size_class, struct slab** bin); 
-struct large_chunk* _allocate_large_bin_chunk(int size, struct large_chunk** bin);
 void _split_large_chunk(struct large_chunk* chunk, int size); 
 void* heap_alloc(size_t bytes); 
-struct large_chunk* _search_large_bin_first_fit(int size); 
 void heap_free(void* ptr);
 void print_large_bin_contents(); 
 int chunk_in_span(struct large_chunk* ref_chunk, struct large_chunk* adj_chunk);
-struct large_chunk* prev_physical_chunk(struct large_chunk* chunk); 
-struct large_chunk* next_physical_chunk(struct large_chunk* chunk); 
 void unlink_large_free_chunk(struct large_chunk* chunk); 
 void insert_large_free_chunk(struct large_chunk* chunk);
 
